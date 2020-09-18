1a34a95e8e283874161177579387443c2faab080
Liang_Xiaowei <38693759+QRr0w@users.noreply.github.com>
2020-09-18 17:35:32 +0800

    Break the Bricks source codes

---
+++ Code/Game Development (Study)/Break the Bricks/Break the Bricks/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(h, v, 0) * Time.deltaTime * speed);
+    }
+}
---
+++ Code/Game Development (Study)/Break the Bricks/Break the Bricks/Assets/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject.Instantiate(bullet,transform.position,transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject ball = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody rgd = ball.GetComponent<Rigidbody>();
+            rgd.velocity = transform.forward * speed;
+
+        }
+    }
+}
